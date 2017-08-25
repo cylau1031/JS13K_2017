d42b5c12e5e37ccf4db07b0af4eb1b7c879f46ef
puhtreeseeya <puhtreeseeya@gmail.com>
2017-08-25 02:38:10 -0400

    ...

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ var gameState = {
 kontra.init() 
 console.log(kontra)
 kontra.assets.imagePath = 'assets/images/'
-kontra.assets.load('player.png')
+kontra.assets.load('player.png', 'cloud.png')
   .then(() => {
+    const canvasHeight = kontra.canvas.height;
+    const canvasWidth = kontra.canvas.width;
+
     let player = kontra.sprite({
       x: 10,
       y: 10,
@@ -15,6 +18,15 @@ kontra.assets.load('player.png')
       update: () => {
         //console.log('updating myself')
       }
+    })
+
+    //I ADDED A CLOUD FOR NO REASON... just playing around 
+    //DONT MIND THIS CLOUD lol -pat
+
+    let cloud = kontra.sprite({
+      x: canvasWidth/4,
+      y: canvasHeight/4,
+      image: kontra.assets.images.cloud
     })
     ///////////////////////////////////////
     //////////////MEMORIES/////////////////
@@ -31,8 +43,7 @@ kontra.assets.load('player.png')
      * towards center
      */
     //calculate dx, dy to travel to center
-    const canvasHeight = kontra.canvas.height;
-    const canvasWidth = kontra.canvas.width;
+    
     function calculateDx(xStart) {
       return ((-2 * xStart + canvasWidth) / canvasWidth);
     }
@@ -110,11 +121,13 @@ kontra.assets.load('player.png')
       },
       render: () => {
         player.render()
+        cloud.render() 
         memories.render()
         // board.render()
       }
     })
 
+    //keypress logic to pause and unpause the game 
     kontra.keys.bind('esc', () => {
       if(!gameState.isPaused) {
         gameState.isPaused = true; 
@@ -124,10 +137,6 @@ kontra.assets.load('player.png')
         loop.start(); 
       }
     }); 
-    // kontra.keys.bind('enter', () => {
-    //   loop.start()
-    // }) 
-
     loop.start()
   })
   .then(() => { })
