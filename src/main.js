09c905e2732513674858a0b6bff20869113c5f43
cylau1031 <cylau1031@gmail.com>
2017-09-07 15:23:29 -0400

    updated objs for player and movement

--- src/main.js
+++ src/main.js
@@ -7,14 +7,14 @@ var gameState = {
 
 function getDirection(position, center) {
   var scale = 0.004; //scales it down so that memories move incremently instead of jumping to the center
-  var random = Math.random() + .25; //randomizes the scaling for mixed speeds 
+  var random = Math.random() + .25; //randomizes the scaling for mixed speeds
   var direction =  { x: (center.x-position.x)*scale*random, y: (center.y - position.y)*scale*random}
-  return direction; 
+  return direction;
 }
 
 
 
-kontra.init() 
+kontra.init()
 console.log(kontra)
 kontra.assets.imagePath = 'assets/images/'
 kontra.assets.load('player.png', 'cloud.png')
@@ -22,7 +22,7 @@ kontra.assets.load('player.png', 'cloud.png')
     const canvasHeight = kontra.canvas.height;
     const canvasWidth = kontra.canvas.width;
 
-    //I ADDED A CLOUD FOR NO REASON... just playing around 
+    //I ADDED A CLOUD FOR NO REASON... just playing around
     //DONT MIND THIS CLOUD lol -pat
 
     let cloud = kontra.sprite({
@@ -31,15 +31,56 @@ kontra.assets.load('player.png', 'cloud.png')
       image: kontra.assets.images.cloud
     })
 
-    let player = kontra.sprite({
-      x: 10,
-      y: 10,
-      image: kontra.assets.images.player,
-      update: () => {
-        //console.log('updating myself')
+    let circle = kontra.sprite({
+      x: 400,
+      y: 300,
+      color: 'red',
+      radius: 100,
+      render: function() {
+        this.context.fillStyle = this.color
+        this.context.beginPath()
+        this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+        this.context.fill()
       }
     })
 
+    let arc = kontra.sprite({
+      x: circle.x,
+      y: circle.y,
+      color: 'white',
+      radius: 110,
+      lineWidth: 6,
+      startAngle: 0,
+      angleLength: (Math.PI * 2)/15,
+      update: function() {
+        if (kontra.keys.pressed('left')) {
+          this.startAngle += (Math.PI * 2)/180
+        }
+        if (kontra.keys.pressed('right')) {
+          this.startAngle -= (Math.PI * 2)/180
+        }
+      },
+      render: function() {
+        this.context.lineWidth = this.lineWidth
+        this.context.beginPath()
+        this.context.arc(this.x, this.y, this.radius, this.startAngle, this.startAngle + this.angleLength)
+        this.context.strokeStyle = this.color
+        this.context.stroke()
+      }
+    })
+
+    let pointKeeper = kontra.sprite({
+      points: 0,
+      x: 100,
+      y: 100,
+      update: function() {
+        //if there was a collision then add points
+      },
+      render: function() {
+        this.context.font = '30px Arial'
+        this.context.fillText(`${this.points}`, this.x, this.y)
+      }
+    })
 
     ///////////////////////////////////////
     //////////////MEMORIES/////////////////
@@ -57,7 +98,7 @@ kontra.assets.load('player.png', 'cloud.png')
      * towards center
      */
     //calculate dx, dy to travel to center
-    
+
     // function calculateDx(xStart) {
     //   return ((-2 * xStart + canvasWidth) / canvasWidth);
     // }
@@ -110,53 +151,45 @@ kontra.assets.load('player.png', 'cloud.png')
     //   }
     // }
 
-
-
-
-
-
-
     const memories = kontra.pool({
       create: kontra.sprite,
       maxSize: MAX_NUM_MEMORIES
     });
 
     function spawnMemories() {
-      var MAX_HEIGHT = kontra.canvas.height-20; 
-      var MAX_WIDTH = kontra.canvas.width-20; 
-      var CANVAS_CENTER = { x: MAX_WIDTH/2, y: MAX_HEIGHT/2 }; 
+      var MAX_HEIGHT = kontra.canvas.height-20;
+      var MAX_WIDTH = kontra.canvas.width-20;
+      var CANVAS_CENTER = { x: MAX_WIDTH/2, y: MAX_HEIGHT/2 };
 
       for(var i=0; i<MAX_NUM_MEMORIES; i++) {
         //toggle is used to determine which side of the board the memories will appear on
         //Math.random is used to determine the toggle
         //essentially '00'=>top, '01'=>bottom, '10'=>left, '11'=>right
         var toggle = '' + Math.round(Math.random()) + Math.round(Math.random());
-        var position; 
+        var position;
         if(toggle === '00') position = { x: Math.random() * MAX_WIDTH, y: 0}
         else if(toggle === '01') position = { x: Math.random() * MAX_WIDTH, y: MAX_HEIGHT}
         else if(toggle === '10') position = { x: 0, y: Math.random() * MAX_WIDTH }
         else if(toggle === '11') position = { x: MAX_WIDTH, y: Math.random() * MAX_HEIGHT}
-        else position = { x: 0, y: 0}   
-        var direction = getDirection(position, CANVAS_CENTER); 
+        else position = { x: 0, y: 0}
+        var direction = getDirection(position, CANVAS_CENTER);
         memories.get({
-          x: position.x, 
-          y: position.y, 
-          dx: direction.x, 
-          dy: direction.y, 
-          color: 'red', 
-          width: 20, 
-          height: 20, 
-          ttl: Infinity, 
-          speed: 0, 
-          type: 'enemy', 
+          x: position.x,
+          y: position.y,
+          dx: direction.x,
+          dy: direction.y,
+          color: 'blue',
+          width: 20,
+          height: 20,
+          ttl: Infinity,
+          speed: 0,
+          type: 'enemy',
           update: function() {
-            this.advance(); 
+            this.advance();
           }
-        }); 
+        });
       }
     }
-    
-    
 
 
     //let board = kontra.sprite({})
@@ -168,6 +201,9 @@ kontra.assets.load('player.png', 'cloud.png')
         if (memories.getAliveObjects().length === 0) {
           spawnMemories();
         }
+        circle.update()
+        arc.update()
+        pointKeeper.update()
 
         //keypress logic
 
@@ -180,29 +216,31 @@ kontra.assets.load('player.png', 'cloud.png')
         // board.update()
       },
       render: () => {
-        player.render()
-        cloud.render() 
+       //player.render()
+        //cloud.render()
         memories.render()
+        circle.render()
+        arc.render()
+        pointKeeper.render()
         // board.render()
       }
     })
 
-    //keypress logic to pause and unpause the game 
+    //keypress logic to pause and unpause the game
     kontra.keys.bind('esc', () => {
       if(!gameState.isPaused) {
-        gameState.isPaused = true; 
-        loop.stop(); 
+        gameState.isPaused = true;
+        loop.stop();
       } else {
-        gameState.isPaused = false; 
-        loop.start(); 
+        gameState.isPaused = false;
+        loop.start();
       }
-    }); 
+    });
 
-    
+
 
     loop.start()
   })
   .then(() => { })
   .catch(console.error)
 
-  
